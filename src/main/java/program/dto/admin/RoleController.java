a8529f2111efa623184c6873c1e2bbd3572abac6
mayevskyivan5@gmail.com <mayevskyivan5@gmail.com>
2022-02-09 19:08:27 +0200

    add cors

--- src/main/java/program/dto/admin/RoleController.java
+++ src/main/java/program/dto/admin/RoleController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import program.dto.admin.roledto.RoleAddDto;
+import program.dto.admin.roledto.RoleDeleteDto;
 import program.entities.Role;
 import program.mapper.ApplicationMapper;
 import program.repositories.RoleRepository;
@@ -18,15 +19,19 @@ public class RoleController {
     private final RoleRepository roleRepository;
     private final ApplicationMapper mapper;
 
-    @GetMapping("")
-
+    @GetMapping("/get")
     public List<Role> list() {
         return roleRepository.findAll();
     }
-    @PostMapping("")
+
+    @PostMapping("/create")
     public int create(RoleAddDto model) {
         Role role = mapper.RoleByAddRoleDto(model);
         roleRepository.save(role);
         return role.getId();
+    }
+    @PostMapping("/delete")
+    public void DeleteRole(RoleDeleteDto Idifiner){
+        roleRepository.deleteById(Integer.parseInt(Idifiner.getId()));
     }
 }
---
+++ src/main/java/program/dto/admin/roledto/RoleDeleteDto.java
@@ -0,0 +1,8 @@
+package program.dto.admin.roledto;
+
+import lombok.Data;
+
+@Data
+public class RoleDeleteDto {
+    private String id;
+}
